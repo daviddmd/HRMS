bfe49461de2c31be0dcab0770c3cd651bd2d0a81
David Duarte <102606398+daviddmd@users.noreply.github.com>
2023-02-06 11:45:54 +0000

    Add ContractDTO to Employee Service, Update Configuration

--- employee-service/src/main/java/com/example/employeeservice/Models/Contract/ContractDTO.java
+++ employee-service/src/main/java/com/example/employeeservice/Models/Contract/ContractDTO.java
@@ -1,4 +1,27 @@
 package com.example.employeeservice.Models.Contract;
 
+
+import com.example.employeeservice.Enums.ContractType;
+import com.example.employeeservice.Models.Jobs.JobDTO;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@SuperBuilder
 public class ContractDTO {
+    private Long id;
+    private LocalDate startingDate;
+    private LocalDate endingDate;
+    private JobDTO job;
+    private BigDecimal salary;
+    private ContractType contractType;
+
+
 }
---
+++ employee-service/src/main/java/com/example/employeeservice/Models/Jobs/JobDTO.java
@@ -0,0 +1,22 @@
+package com.example.employeeservice.Models.Jobs;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class JobDTO {
+    private Long id;
+    private String title;
+    private String description;
+    private BigDecimal minimumSalary;
+    private BigDecimal maximumSalary;
+    private String recruiterEmail;
+
+}
