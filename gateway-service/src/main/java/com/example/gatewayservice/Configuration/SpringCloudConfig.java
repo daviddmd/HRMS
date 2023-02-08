26be2705904a86bc7ab8d4059530fd83a9277b65
David Duarte <102606398+daviddmd@users.noreply.github.com>
2023-02-08 21:45:12 +0000

    Add Spring Cloud Gateway Keycloak authentication and authorization support
    Replace Bean-Based Gateway route configuration by YAML

--- gateway-service/src/main/java/com/example/gatewayservice/Configuration/SpringCloudConfig.java
+++ gateway-service/src/main/java/com/example/gatewayservice/Configuration/SpringCloudConfig.java
@@ -1,25 +1,51 @@
 package com.example.gatewayservice.Configuration;
-
-import org.springframework.cloud.gateway.route.RouteLocator;
-import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableReactiveMethodSecurity;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
 
 @Configuration
+@EnableWebFluxSecurity
+@EnableReactiveMethodSecurity
 public class SpringCloudConfig {
+    /*
+    @Autowired
+    private TokenRelayGatewayFilterFactory filterFactory;
+
     @Bean
     public RouteLocator gatewayRoutes(RouteLocatorBuilder builder) {
         return builder.routes()
                 .route(r -> r.path("/employees/**")
+                        .filters(f -> f.filter(filterFactory.apply()))
                         .uri("lb://employee-service")
                 )
 
                 .route(r -> r.path("/departments/**", "/locations/**")
+                        .filters(f -> f.filter(filterFactory.apply()))
                         .uri("lb://department-service")
                 )
                 .route(r -> r.path("/jobs/**", "/contracts/**")
+                        .filters(f -> f.filter(filterFactory.apply()))
                         .uri("lb://job-service")
                 )
                 .build();
     }
+     */
+    @Bean
+    public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
+        http
+                .authorizeExchange()
+                .anyExchange().
+                authenticated()
+                .and()
+                .csrf().disable()
+                .oauth2Login()
+                .and()
+                .oauth2ResourceServer()
+                .jwt();
+        return http.build();
+    }
+
 }
